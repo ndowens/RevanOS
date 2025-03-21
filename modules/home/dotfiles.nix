0ef724af82db8c0036d4cba2025b7fe62fc91690
ndowens <ndowens@artixlinux.org>
2025-03-21 16:17:08 -0500

    Add artix related stuff

--- modules/home/dotfiles.nix
+++ modules/home/dotfiles.nix
@@ -1,5 +1,8 @@
 {
   home.file = {
     ".config/artools".source = ./configs/artools;
+    ".config/pacman.d".source = ./configs/pacman.d;
+    ".config/pacman.conf".source = ./configs/pacman.conf;
+    ".config/makepkg.conf".source = ./configs/makepkg.conf;
   };
 }

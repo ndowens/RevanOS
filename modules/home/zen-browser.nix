4575362b69c7414f2a5855cbe2987b15aed3037e
ndowens <ndowens@artixlinux.org>
2025-03-21 19:04:08 -0500

    zen-browser

--- modules/home/zen-browser.nix
+++ modules/home/zen-browser.nix
@@ -1,11 +1,6 @@
+{inputs, pkgs, ...}:
 {
-  inputs = {
-    zen-browser.url = "github:0xc000022070/zen-browser-flake";
-  };
-  outputs = {pkgs, home-manager, zen-browser,...} @ inputs:
-  {
     home.packages = with pkgs; [
-    inputs.zen-browser.packages."${system}".default;
+      inputs.zen-browser.packages."${system}".default
     ];
-  };
 }

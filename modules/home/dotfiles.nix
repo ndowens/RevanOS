12208f066a175716b823541ad28e04c40555c2e3
ndowens <ndowens@artixlinux.org>
2025-04-01 23:55:08 -0500

    Add artix-checkupdates

--- modules/home/dotfiles.nix
+++ modules/home/dotfiles.nix
@@ -4,5 +4,6 @@
     ".config/pacman.d".source = ./configs/pacman.d;
     ".config/pacman.conf".source = ./configs/pacman.conf;
     ".config/makepkg.conf".source = ./configs/makepkg.conf;
+    ".config/artix-checkupdates/config".source = ./configs/artix-checkupdates/config;
   };
 }

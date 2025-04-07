bef07f8bb2e564d00de59829f7619e5e6de91e59
ndowens <ndowens@artixlinux.org>
2025-04-06 22:55:03 -0500

    Add ESO

--- modules/home/dotfiles.nix
+++ modules/home/dotfiles.nix
@@ -1,9 +1,10 @@
-{
+{username, ...}: {
   home.file = {
     ".config/artools".source = ./configs/artools;
     ".config/pacman.d".source = ./configs/pacman.d;
     ".config/pacman.conf".source = ./configs/pacman.conf;
     ".config/makepkg.conf".source = ./configs/makepkg.conf;
     ".config/artix-checkupdates/config".source = ./configs/artix-checkupdates/config;
+    "/home/${username}/Documents/'Elder Scrolls Online'".source = ./configs/Elder;
   };
 }

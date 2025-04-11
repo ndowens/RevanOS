6f2b774d14dec61d0e8da919be916e3027daa36a
ndowens <ndowens@artixlinux.org>
2025-04-11 02:59:48 -0500

    Add missing -H switch in nh alias

--- modules/home/zsh/default.nix
+++ modules/home/zsh/default.nix
@@ -58,9 +58,9 @@
       vim = "nvim";
       am = "artix-metro";
       c = "clear";
-      fc = "nh os switch ${profile} -u --ask";
-      fr = "nh os switch ${profile}";
-      fu = "nh os switch ${profile} -u";
+      fc = "nh os switch -H ${profile} -u --ask";
+      fr = "nh os switch -H ${profile}";
+      fu = "nh os switch -H ${profile} -u";
       ncg = "nix-collect-garbage --delete-old && sudo nix-collect-garbage -d && sudo /run/current-system/bin/switch-to-configuration boot";
       cat = "bat";
       man = "batman";

7799e72464b7d1051d9553d3dca4e22277b1cde1
ndowens <ndowens@artixlinux.org>
2025-04-25 17:40:06 -0500

    Add lunar client

--- modules/home/packages.nix
+++ modules/home/packages.nix
@@ -2,16 +2,16 @@
   pkgs,
   inputs,
   ...
-}: {
+}:
+{
   home.packages = with pkgs; [
-    easyrpg-player
     heroic
-    xfce.thunar
     legcord
     gst_all_1.gst-libav
     unzip # Needed for ESO price updater
     keepassxc
     nix-tree
+    lunar-client
     inputs.nixvim.packages.x86_64-linux.default
   ];
 }
